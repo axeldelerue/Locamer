d4a17937a1f211c8a75ea2d6043054084fb8f2c2
delerue.axel <delerue.axel@R20914>
2017-05-22 09:23:40 +0200

    Généartion Controlleur et View tarif

---
+++ WebLocamer/Controllers/tarifsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebLocamer.Models;
+
+namespace WebLocamer.Controllers
+{
+    public class tarifsController : Controller
+    {
+        private LocamerEntities db = new LocamerEntities();
+
+        // GET: tarifs
+        public ActionResult Index()
+        {
+            return View(db.tarifs.ToList());
+        }
+
+        // GET: tarifs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tarif tarif = db.tarifs.Find(id);
+            if (tarif == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarif);
+        }
+
+        // GET: tarifs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tarifs/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "libelle,id_tarif,prix_jour")] tarif tarif)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tarifs.Add(tarif);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tarif);
+        }
+
+        // GET: tarifs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tarif tarif = db.tarifs.Find(id);
+            if (tarif == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarif);
+        }
+
+        // POST: tarifs/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "libelle,id_tarif,prix_jour")] tarif tarif)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tarif).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tarif);
+        }
+
+        // GET: tarifs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tarif tarif = db.tarifs.Find(id);
+            if (tarif == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarif);
+        }
+
+        // POST: tarifs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tarif tarif = db.tarifs.Find(id);
+            db.tarifs.Remove(tarif);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
