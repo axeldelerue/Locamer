f772f2a5908b58e75cf243fffff285edc787286b
delerue.axel <delerue.axel@R20914>
2017-05-22 08:25:34 +0200

    Ajoutez des fichiers projet.

---
+++ WebLocamer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebLocamer.MvcApplication" Language="C#" %>
---
+++ WebLocamer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebLocamer.Startup))]
+namespace WebLocamer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
